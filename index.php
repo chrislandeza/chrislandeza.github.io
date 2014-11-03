18522d03376088db1ea5bd27c1c569dbe7a242c7
chrislandeza <CC_Landeza@yahoo.com>
2014-11-03 17:26:52 +0800

    Crafting Timeline

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
     <section id="navigation">
         <!-- Navigation -->
         <nav id="nav" class="navbar navbar-default navbar-static-top" role="navigation">
-            <div class="container">
+            <!-- <div class="container"> -->
                 <div class="navbar-header page-scroll">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                         <span class="sr-only">Toggle navigation</span>
@@ -89,7 +89,7 @@
                             <a class="page-scroll" href="#profile" class="about-section">Profile</a>
                         </li>
                         <li>
-                            <a class="page-scroll" href="#experiences">Experiences</a>
+                            <a class="page-scroll" href="#experiences">Menu</a>
                         </li>
                         <li>
                             <a class="page-scroll" href="#contact">Contact</a>
@@ -98,7 +98,7 @@
                     </ul>
                 </div>
                 <!-- /.navbar-collapse -->
-            </div>
+            <!-- </div> -->
             <!-- /.container -->
         </nav>
         
@@ -110,7 +110,7 @@
     <section id="profile" class="full-height">
         <div class="container">
             
-            <h2 class="heading wow fadeInDown">Profile</h2>
+            <h2 class="heading wow fadeInDown">Shangri-La</h2>
             <p class="lead text-center wow fadeInDown">Shangri-La Finest Chinese Cuisine</p>
             <hr>
             <div class="row">
@@ -119,7 +119,9 @@
                     <p>I am an allround web developer. I am a senior programmer with good knowledge of front-end techniques. I love structure and order and I also stand for quality. I love spending time on fixing little details and optimizing web apps. Also I like working in a team, you'll learn faster and more. As the saying goes: 'two heads are better than one'.</p>
                 </div>
                 <div class="col-md-4 wow fadeInLeft">
-                    <img src="assets/img/tester.png" class="img-responsive" >
+                <center>
+                    <img src="assets/img/tester.png" class="img-responsive img-centered" >
+                </center>
                 </div>
                 <div class="col-md-4 wow fadeInLeft">
                     <h3>Details</h3>
@@ -133,7 +135,6 @@
                     <dd >Valenzuela City, Philippines</dd>
                 </dl>
                     
-                    
                 </div>
             </div>
              
@@ -145,8 +146,104 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Services Section</h1>
-                    <h2>Shangri-La Finest Chinese Cuisine</h2>
+                    <h1>Shangri-La Timeline</h1>
+                    <div class="container">
+                            <div class="page-header">
+                                <h1 id="timeline">Timeline</h1>
+                            </div>
+                            <ul class="timeline">
+                                <li>
+                                  <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                      <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+                                    </div>
+                                    <div class="timeline-body">
+                                      <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                                    </div>
+                                  </div>
+                                </li>
+                                <li class="timeline-inverted">
+                                  <div class="timeline-badge warning"><i class="glyphicon glyphicon-credit-card"></i></div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                    </div>
+                                    <div class="timeline-body">
+                                      <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                                      <p>Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.</p>
+                                    </div>
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="timeline-badge danger"><i class="glyphicon glyphicon-credit-card"></i></div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                    </div>
+                                    <div class="timeline-body">
+                                      <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                                    </div>
+                                  </div>
+                                </li>
+                                <li class="timeline-inverted">
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                    </div>
+                                    <div class="timeline-body">
+                                      <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                                    </div>
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="timeline-badge info"><i class="glyphicon glyphicon-floppy-disk"></i></div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                    </div>
+                                    <div class="timeline-body">
+                                      <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                                      <hr>
+                                      <div class="btn-group">
+                                        <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">
+                                          <i class="glyphicon glyphicon-cog"></i> <span class="caret"></span>
+                                        </button>
+                                        <ul class="dropdown-menu" role="menu">
+                                          <li><a href="#">Action</a></li>
+                                          <li><a href="#">Another action</a></li>
+                                          <li><a href="#">Something else here</a></li>
+                                          <li class="divider"></li>
+                                          <li><a href="#">Separated link</a></li>
+                                        </ul>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                    </div>
+                                    <div class="timeline-body">
+                                      <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                                    </div>
+                                  </div>
+                                </li>
+                                <li class="timeline-inverted">
+                                  <div class="timeline-badge success"><i class="glyphicon glyphicon-thumbs-up"></i></div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                                    </div>
+                                    <div class="timeline-body">
+                                      <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                                    </div>
+                                  </div>
+                                </li>
+                            </ul>
+                        </div>
                 </div>
             </div>
         </div>
