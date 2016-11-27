374753525047f89628f697744d56141391c87003
Chris Landeza <CC_Landeza@yahoo.com>
2016-11-27 16:02:58 +0800

    Fresh jigsaw installation

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,20 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
+var argv = require('yargs').argv;
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir.config.assetsPath = 'source/_assets';
+elixir.config.publicPath = 'source';
 
+elixir(function(mix) {
+    var env = argv.e || argv.env || 'local';
+    var port = argv.p || argv.port || 3000;
 
-elixir.config.publicDir = '';
-elixir.config.cssOutput = 'assets/css';
-elixir.config.jsOutput = 'assets/js';
-
-elixir(function (mix) {
-
-    mix.styles([
-        'bootstrap.min.css', 
-        'font-awesome.min.css', 
-        'animate.css', 
-        'custom.css'
-    ], './assets/css/all.css');
-
-    mix.scripts([
-        'jquery-1.11.0.js', 
-        'bootstrap.min.js', 
-        'jquery.easing.min.js', 
-        'main.js', 
-        'jquery.sticky.js', 
-        'wow.min.js'
-    ], './assets/js/all.js');
-
-
-
-
-
+    mix.sass('main.scss')
+        .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
+        .browserSync({
+            port: port,
+            server: { baseDir: 'build_' + env },
+            proxy: null,
+            files: [ 'build_' + env + '/**/*' ]
+        });
 });
